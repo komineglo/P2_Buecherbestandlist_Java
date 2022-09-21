72b206ca74920eb2ffc74a34e0e29f44af848e2c
Yves Komi NEGLO <37405709+komineglo@users.noreply.github.com>
2022-09-21 23:02:45 +0200

    Add files via upload

---
+++ Prog2Projekt/src/prog2projekt/Prog2Projekt.java
@@ -0,0 +1,10 @@
+package prog2projekt;
+
+public class Prog2Projekt {
+    
+    public static void main(String[] args) {
+        Table tb = new Table();
+        tb.setVisible(true);
+    }
+    
+}
